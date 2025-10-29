42fec7eb8d7e980280cb827ada22d768a2b478f7
noambezalel12345 <noambezalel2008@gmail.com>
2025-10-29 15:05:50 +0200

    initial commit

---
+++ app/src/main/java/com/example/drivesmart/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.drivesmart;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        EditText input = findViewById(R.id.inputText);
+        TextView output = findViewById(R.id.outputText);
+
+        button.setOnClickListener(v -> {
+           output.setText(input.getText());
+        });
+
+    }
+}
+
